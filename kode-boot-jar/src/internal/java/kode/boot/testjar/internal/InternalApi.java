14d178de4c358d658fec72083b14fd599662b2e7
kostark <10692081@qq.com>
2016-07-11 23:04:01 +0800

    2016/07/11

---
+++ kode-boot-jar/src/internal/java/kode/boot/testjar/internal/InternalApi.java
@@ -0,0 +1,13 @@
+package kode.boot.testjar.internal;
+
+import kode.boot.testjar.config.DatabaseConfig;
+
+/**
+ * @author Stark
+ * @since 1.0, 2016/7/11
+ */
+public class InternalApi {
+	public static final String TAG = "internal";
+
+	DatabaseConfig dc;
+}
