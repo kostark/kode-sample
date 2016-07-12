82a526a92f539d1c6430e32e417672723ef7b212
kostark <10692081@qq.com>
2016-07-12 23:40:01 +0800

    Rename application and deploy war configured by application.java

---
+++ kode-boot-jar/src/main/java/kode/boot/testjar/Application.java
@@ -0,0 +1,27 @@
+package kode.boot.testjar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * 应用起始类，SpringBootServletInitializer 是为了支持 war 包启动。
+ *
+ * @author Stark
+ * @since 1.0, 2016/7/6
+ */
+@EnableAutoConfiguration
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return super.configure(builder);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
