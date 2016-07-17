9c1c8385e5c212091c0f888c80cce5361fd03801
kostark <10692081@qq.com>
2016-07-18 01:05:09 +0800

    2016/07/17

--- kode-boot-jar/src/main/java/kode/boot/testjar/controller/HomeController.java
+++ kode-boot-jar/src/main/java/kode/boot/testjar/controller/HomeController.java
@@ -5,6 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.constraints.Future;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
 /**
  * @version 1.0
  * @since 2016/7/7
@@ -35,6 +40,16 @@ public class HomeController {
 	String test(Model model) {
 		model.addAttribute("message", "Hello world! ============== test sample ||| " + desc + " | test = " + test + " | devName = ");
 		return "test";
+	}
+
+	String sample(
+			@NotNull
+			@Min(0)
+					Long id,
+			@NotNull
+			@Future
+					Date date) {
+		return "";
 	}
 
 }
